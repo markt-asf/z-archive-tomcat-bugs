1ef70506c5f35821b6f67d33ff00939994a7c665
Mark Thomas <markt@apache.org>
2018-07-26 18:04:47 +0200

    Test simple error response

---
+++ src/java/org/apache/tomcat/User001.java
@@ -0,0 +1,20 @@
+package org.apache.tomcat;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/user001")
+public class User001 extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.sendError(500, "Application error message");
+    }
+}
