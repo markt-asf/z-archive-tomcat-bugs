1968367012ee72a1612521579d5b0fd151885504
Mark Thomas <markt@apache.org>
2019-10-17 16:40:56 +0100

    Add mapping

--- src/java/org/apache/tomcat/Bug49884.java
+++ src/java/org/apache/tomcat/Bug49884.java
@@ -7,10 +7,12 @@ import java.util.concurrent.Executors;
 
 import javax.servlet.AsyncContext;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet(asyncSupported = true, urlPatterns = "/bug49884")
 public class Bug49884 extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
